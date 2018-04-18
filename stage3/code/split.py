3cfef2204b249a25a2239e6db745f85361db0a9e
謝沅廷 <yuantinghsieh@xieyuantingde-MacBook-Pro.local>
2018-04-17 22:41:03 -0500

    add block normalize ipynb

--- stage3/code/split.py
+++ stage3/code/split.py
@@ -5,7 +5,6 @@ import os
 DATA_DIR = '../data'
 
 samples = pd.read_csv(os.path.join(DATA_DIR, 'samples.csv'))
-np.random.seed(7)
 
 NUM_SAMPLES = samples.shape[0]
 PEOPLE = 3
@@ -13,8 +12,8 @@ PEOPLE = 3
 #index = np.random.permutation(block.shape[0])[0:NUM_SAMPLES]
 index = range(samples.shape[0])
 for i in range(PEOPLE):
-  sample = samples.iloc[index[0 + i*NUM_SAMPLES/PEOPLE: (i+1)*NUM_SAMPLES/PEOPLE], :]
+  step = int(NUM_SAMPLES/PEOPLE)
+  sample = samples.iloc[index[0 + i*step : (i+1)* step], :]
   filename = 'sample_' + str(i) + '.csv'
   sample.to_csv(os.path.join(DATA_DIR, filename))
-
 
